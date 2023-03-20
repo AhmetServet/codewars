b84bf9338e4026228f73bd2ce1d26d4951850c06
ahmetservet <ahmetservet04@gmail.com>
2023-03-20 16:48:57 +0300

    Solution for Stop gninnipS My sdroW! Problem

---
+++ spinning-words.py
@@ -0,0 +1,11 @@
+def spin_words(sentence):
+    words = sentence.split(" ")
+    new_words = []
+
+    for word in words:
+        if len(word) >= 5:
+            new_words.append(word[::-1])
+        else:
+            new_words.append(word)
+
+    return " ".join(new_words)
